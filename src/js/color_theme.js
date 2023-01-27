63e9204391f656f7b4a4afbe0b98c564b1caa239
Anka <103377387+ann153476@users.noreply.github.com>
2023-01-27 03:30:19 +0200

    new

--- src/js/color_theme.js
+++ src/js/color_theme.js
@@ -8,15 +8,19 @@ const searchInput = document.querySelector('#search-input');
 
 
 function colorTheme(){
-    body.classList.toggle('light');
-    body.classList.toggle('dark');
-    svgLight.classList.toggle('light__svg');
-    svgLight.classList.toggle('dark__svg');
-    svgDark.classList.toggle('light__svg');
-    svgDark.classList.toggle('dark__svg');  
-    svgSearch.classList.toggle('search__theme__light');
-    svgSearch.classList.toggle('search__theme__dark');
-    searchInput.classList.toggle('search__theme__light');
-    searchInput.classList.toggle('search__theme__dark');
+    body.classList.toggle('light__theme');
+    body.classList.toggle('dark__theme');//body
+
+    svgLight.classList.toggle('svg__light__theme');
+    svgLight.classList.toggle('svg__dark__theme');//sun//light
+
+    svgDark.classList.toggle('svg__light__theme');
+    svgDark.classList.toggle('svg__dark__theme');//moon//dark
+
+    svgSearch.classList.toggle('svg__search__light__theme');
+    svgSearch.classList.toggle('svg__search__dark__theme');//лупа
+    
+    searchInput.classList.toggle('input__seatch__light__theme');
+    searchInput.classList.toggle('input__seatch__dark__theme');//input
 
 }
